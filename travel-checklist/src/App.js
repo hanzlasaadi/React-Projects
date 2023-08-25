3d52481796ac40f7cf73184957609d4280088f0d
hanzlasaadi <hanzalasaadi@gmail.com>
2023-08-25 19:17:18 +0500

    render packing list

--- travel-checklist/src/App.js
+++ travel-checklist/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -16,8 +22,28 @@ function Form() {
   return <div className="add-form">What do you need for your trip???</div>;
 }
 function PackingList() {
-  return <div className="list">List!</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((obj) => (
+          <Item item={obj} key={obj.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
