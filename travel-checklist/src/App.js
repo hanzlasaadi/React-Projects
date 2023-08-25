73decd8482deb7836397861b47575984905538a1
hanzlasaadi <hanzalasaadi@gmail.com>
2023-08-25 21:59:57 +0500

    controlled elements

--- travel-checklist/src/App.js
+++ travel-checklist/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,22 +22,40 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    if (!description) return;
+
+    const listItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(listItem);
+
+    // reset input/select values
+    setDescription("");
+    setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip???</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
           <option value={i} key={i}>
             {i}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
