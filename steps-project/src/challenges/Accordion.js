a4add037c77dd4b2ff9b3bc87f16dd652fe07446
hanzlasaadi <hanzalasaadi@gmail.com>
2023-08-27 19:58:41 +0500

    accordion exercise part#1

---
+++ steps-project/src/challenges/Accordion.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      {faqs.map((faq, i) => (
+        <Item number={i + 1} title={faq.title} key={faq.title}>
+          {faq.text}
+        </Item>
+      ))}
+    </div>
+  );
+}
+
+function Item({ number, title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <p className="number">{number < 10 ? `0${number}` : number}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
