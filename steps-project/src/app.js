600b355549fedcf3e8ed2805364157f26a30b651
hanzlasaadi <hanzalasaadi@gmail.com>
2023-08-27 17:43:51 +0500

    reusabe components with children props

--- steps-project/src/app.js
+++ steps-project/src/app.js
@@ -56,26 +56,42 @@ function Steps() {
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              handleFunc={handlePrevious}
             >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+              👈 Previous
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" handleFunc={handleNext}>
+              Next 👉
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ bgColor, textColor, handleFunc, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={handleFunc}
+    >
+      {children}
+    </button>
   );
 }
