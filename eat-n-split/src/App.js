83afcea63688a0e98510d364929b8469854b8758
hanzlasaadi <hanzalasaadi@gmail.com>
2023-08-28 20:41:20 +0500

    eat-n-split project setup

---
+++ eat-n-split/src/App.js
@@ -0,0 +1,63 @@
+// import uniqid from "uniqid";
+
+const initialFriends = [
+  {
+    id: "llv0w93w",
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: "llv10ot8",
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: "llv117nf",
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  return (
+    <ul>
+      {initialFriends.map((fr) => (
+        <Friend friend={fr} key={fr.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 ? (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      ) : friend.balance > 0 ? (
+        <p className="green">
+          {friend.name} owes you ${friend.balance}
+        </p>
+      ) : (
+        <p>You and {friend.name} are even`</p>
+      )}
+      <button className="button">Select</button>
+    </li>
+  );
+}
