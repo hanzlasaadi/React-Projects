feddd1fa40723223df69aa2644926abded3a93aa
hanzlasaadi <hanzalasaadi@gmail.com>
2023-09-14 22:45:19 +0500

    challenge textExpander

--- popcornTime/src/index.js
+++ popcornTime/src/index.js
@@ -1,31 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./app";
-import Star from "./StarRating";
-function Test() {
-  const defaultRating = 0;
-  const [rated, setRated] = useState(defaultRating);
-  return (
-    <div>
-      <Star maxRating={10} color="purple" setRated={setRated} />
-      <p>Hello World, Rating is {rated}/10</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./app";
+
+// import Star from "./challenges/StarRating";
+// import Expander from "./challenges/Expander";
 
 // react v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Star
-      maxRating={5}
-      color="blue"
-      size={48}
-      messages={["shit", "meh", "avg", "nice", "fuck yeah"]}
-      defaultRating={3}
-    />
-    <Test />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
