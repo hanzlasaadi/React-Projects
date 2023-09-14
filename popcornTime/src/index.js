cf082866b086a06f3ce87196513749c3c7f01eec
hanzlasaadi <hanzalasaadi@gmail.com>
2023-09-14 19:33:37 +0500

    reusability with props

--- popcornTime/src/index.js
+++ popcornTime/src/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./app";
 import Star from "./StarRating";
+function Test() {
+  const defaultRating = 0;
+  const [rated, setRated] = useState(defaultRating);
+  return (
+    <div>
+      <Star maxRating={10} color="purple" setRated={setRated} />
+      <p>Hello World, Rating is {rated}/10</p>
+    </div>
+  );
+}
 
 // react v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Star num={10} />
+    <Star
+      maxRating={5}
+      color="blue"
+      size={48}
+      messages={["shit", "meh", "avg", "nice", "fuck yeah"]}
+      defaultRating={3}
+    />
+    <Test />
     {/* <App /> */}
   </React.StrictMode>
 );
